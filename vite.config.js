5bf409d740f85448610ffe6491c00ad99782db98
Mohamed Kilani <mohkil87@gmail.com>
2025-05-31 05:19:15 +1000

    feat: Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis'
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+      stream: 'stream-browserify', // <-- ADD THIS LINE
+    },
+  },
 })
