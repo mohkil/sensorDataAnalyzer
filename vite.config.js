b21b50aa830f15bfa5a301cd1801899fde6523fc
Mohamed Kilani <mohkil87@gmail.com>
2025-05-31 05:27:07 +1000

    feat: Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sensorDataAnalyzer/', // <-- ADD OR MODIFY THIS LINE
   define: {
     global: 'globalThis'
   },
   resolve: {
     alias: {
       buffer: 'buffer/',
-      stream: 'stream-browserify', // <-- ADD THIS LINE
+      stream: 'stream-browserify',
     },
   },
 })
